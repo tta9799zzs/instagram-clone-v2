752f8e882e51dc531cf74d70c487c16770bb0dce
tta9799zzs <tta9799@gmail.com>
2021-02-10 22:00:14 +0700

    first

---
+++ src/main/java/com/anhtrt/instaclonev2/InstaCloneV2Application.java
@@ -0,0 +1,13 @@
+package com.anhtrt.instaclonev2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaCloneV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstaCloneV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/anhtrt/instaclonev2/InstaCloneV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anhtrt.instaclonev2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaCloneV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
